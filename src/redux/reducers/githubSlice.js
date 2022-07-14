deffcdf00ec6a28837b2f2d341c45b0794139953
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-14 21:52:11 +0100

    fetch external api with create AsynvThunk

--- src/redux/reducers/githubSlice.js
+++ src/redux/reducers/githubSlice.js
@@ -1,21 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
-// import {githubAPI} from '../../Api/api';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import { githubAPI } from '../../Api/api';
+import axios from 'axios';
+// import { getGithhub } from '../redux/reducers/githubSlice';
+
+
+export const fetchAsyncGithub = createAsyncThunk("github/fetchAsyncGithub", async () => {
+	try {
+		const response = await axios.get('https://api.github.com/search/repositories?q=created:>2021-08-13&sort=stars&order=desc');
+		return response.data;
+	}catch(error) {
+		return error;
+	}
+});
 
 const initialState = {
 	github: {},
+	isLoading: false,
 };
 
 const githubSlice = createSlice({
 	name: 'github',
 	initialState,
-	reducers: {
-		getGithhub: (state,{ payload }) => {
-			state.github = payload;
-		}
-	}
+	
 });
 
 // console.log(githubSlice);
-export const {getGithhub} = githubSlice.actions;
-export const getAllGithub = (state) => state.github;
+// export const {getGithhub} = githubSlice.actions;
+export const getAllGithub = (state) => state.github.github;
+console.log(getAllGithub);
 export default githubSlice.reducer;
