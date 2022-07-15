0f9632c50b9251f37d0cab587175eed1ab1392cc
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-16 00:37:13 +0100

    Import icons compnent in Navbar component

---
+++ src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {LeftIcon, RightIcon} from '../icons';
+
+const Navbar = () => {
+
+
+	return (
+		<div>
+			<header className="header">
+				<div className="heroicon">
+					<LeftIcon />
+					<RightIcon />
+				</div>
+				<div className="form-contain">
+					
+				</div>
+			</header>
+		</div>
+	)
+}
+
+export default Navbar
