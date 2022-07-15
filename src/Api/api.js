67038068711ad203df5eeea14be9455fec803eab
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-15 16:23:55 +0100

    Add API constants with Api endpoints and Api base

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,10 +1,6 @@
-import axios from 'axios';
-
-export const githubAPI = async () => {
-	try {
-		const response = await axios.get('https://api.github.com/search/repositories?q=created:>2021-08-13&sort=stars&order=desc');
-		return response;
-	}catch(error) {
-		return error;
-	}
-}
+export const API_ENDPOINT = "https://api.github.com/search/repositories?q=created:>2021-08-13&sort=stars&order=desc"
+export const API_BASE = 'https://api.github.com/search/repositories';
+export const PARAM_SEARCH = 'q=created:>';
+export const API_SORT = 'sort=stars';
+export const PARAM_ORDER = 'order=desc';
+export const PARAM_PAGE = 'page=';
