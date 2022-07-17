109da03cbe49bad68c57cf71e4cacfa92236782a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-17 01:19:17 +0100

    Destructure Api date

---
+++ src/containers/GithubCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GithubCard = ({name,owner,description,html_url,open_issues_count,stargazers_count}) => {
+
+	return (
+		<ul className="ul-contain">
+			
+		</ul>
+	)
+}
+
+export default GithubCard
