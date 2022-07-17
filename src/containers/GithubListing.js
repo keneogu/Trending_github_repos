b28612cc347bd9ce7fe746e27160be0150a15f2f
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-17 01:20:48 +0100

    Map data to display it on the screen

--- src/containers/GithubListing.js
+++ src/containers/GithubListing.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import { useSelector } from "react-redux";
 import { getAllGithub } from '../redux/reducers/githubSlice';
-
+import GithubCard from './GithubCard';
 import '../App.css';
 
 const GithubListing = () => {
 	const { github } = useSelector(getAllGithub);
 
+	let content;
+	content = github.incomplete_results === true ? (
+		github.items.map((item,i) => (
+			<GithubCard key={i} {...item} />
+		))
+	) : github.isLoading  ? (
+		<div>
+			<h3>...Loading</h3>
+		</div>
+	) : (
+		<h3>...Loading</h3>
+	);
 
 	return (
 		<div className="git-contain">
-			
+			<h2>Github Repo</h2>
+			<h5>with the most stared repositories</h5>
+			<div className="repo-contain">
+				<div className="repo-contain-1">
+					{content}
+				</div>
+			</div>
 		</div>
 	)
 }
