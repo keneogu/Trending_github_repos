5bd10762bead56a0e0cf7f007d0389fe2a01165d
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-07-17 01:20:02 +0100

    Add the Api data to html attributes and tags

--- src/containers/GithubCard.js
+++ src/containers/GithubCard.js
@@ -4,7 +4,18 @@ const GithubCard = ({name,owner,description,html_url,open_issues_count,stargazer
 
 	return (
 		<ul className="ul-contain">
-			
+			<li className="img-contain">
+				<img src={owner.avatar_url} alt="" className="img" />
+			</li>
+			<ul className="ul-list-contain">
+				<li><a href={html_url}>{name}</a></li>
+				<li><p>{description}</p></li>
+				<ul className="last-ul">
+					<li className="last-ul-li">Issues: {open_issues_count}</li>
+					<li className="last-ul-li">Star No: {stargazers_count}</li>
+					<li className="last-ul-li-1">Submitted by: {owner.login}</li>
+				</ul>
+			</ul>
 		</ul>
 	)
 }
